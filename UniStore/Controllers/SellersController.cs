1a6215e54834080999f2b62e183b3b9ce0bb4fde
ROMULO REIS DE OLIVEIRA <01421513005@senacrs.edu.br>
2025-04-28 21:49:09 -0300

    quarto commit

---
+++ UniStore/Controllers/SellersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using UniStore.Data;
+using UniStore.Models;
+
+namespace UniStore.Controllers
+{
+    public class SellersController : Controller
+    {
+        public readonly UniStoreContext _context;
+
+        public SellersController(UniStoreContext context) { 
+            _context = context;
+        }
+
+        /*localhost/Sellers/Index*/
+        public IActionResult Index()
+        {
+            /*List<Seller> sellers = _context.Seller.ToList();*/
+            var sellers = _context.Seller.ToList();
+            return View(sellers);
+        }
+    }
+}
