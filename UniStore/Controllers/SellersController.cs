9ac95ae7a797ec5407018ab7a855030e4eaee932
ROMULO REIS DE OLIVEIRA <01421513005@senacrs.edu.br>
2025-05-05 21:55:22 -0300

    create seller

--- UniStore/Controllers/SellersController.cs
+++ UniStore/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using System.Reflection.Metadata.Ecma335;
 using UniStore.Data;
 using UniStore.Models;
+using UniStore.Models.ViewModels;
 
 namespace UniStore.Controllers
 {
@@ -20,5 +21,35 @@ namespace UniStore.Controllers
             var sellers = _context.Seller.ToList();
             return View(sellers);
         }
+
+        [HttpGet]
+        public IActionResult Create() {
+            //Instanciar um SellerFormViewModel
+            //Essa instância vai ter 2 propriedades
+            //um vendedor e uma lista de departamentos
+            var viewModel = new SellerFormViewModel();
+            viewModel.Departments = _context.Department.ToList();
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Create(Seller seller) { 
+            //Testa se foi passado um objeto Seller
+            if (seller == null)
+            {
+                //Retorna Página não encontrada
+                return NotFound();
+            }
+            //Retorna o primeiro registro de departamento
+            //seller.Department = _context.Department.FirstOrDefault();
+            //seller.DepartmentId = seller.Department.Id;
+            //Adiciona o objeto vendedor (seller) ao banco
+            //_context.Seller.Add(seller);
+            //Adiciona objeto vendedor ao banco
+            _context.Add(seller);
+            //Confirma/Persiste as alterações na base de dados
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
